87e93b4a56109e853f0d3368ae229f1c1a3ecdd8
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-24 04:38:55 +0300

    json da bulunan veriyi word.jsx e cektik

--- src/pages/word.jsx
+++ src/pages/word.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style/word.css";
+import words from "../data/words.json";
 
 const Word = () => {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    setdata(words.kelimeler);
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <div className="word">
       <h1>Word sayfası</h1>
